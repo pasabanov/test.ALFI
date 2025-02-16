07cc8fe5962e722c4b7714ca91454e69a11db7f3
Petr Sabanov <pasabanov@murena.io>
2025-02-16 05:03:20 +0300

    Added evaluation functions for rational functions
    - Replaced `RationalFunction` class with an alias for `std::pair<Container<Number>, Container<Number>>`.
    - Added `alfi::util::numeric::binpow` function for binary exponentiation.
    - Added `val`, `val_mul`, and `val_div` functions in `alfi::ratf` for scalars and vectors.

--- ALFI/ALFI/ratf.h
+++ ALFI/ALFI/ratf.h
@@ -1,40 +1,97 @@
 #pragma once
 
+#include <iostream>
+#include <cmath>
+
 #include "config.h"
+#include "util/numeric.h"
 
 namespace alfi::ratf {
-	template <typename Number, template <typename, typename...> class Container = DefaultContainer>
-	class RationalFunction {
-	public:
-		Container<Number> num;
-		Container<Number> den;
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	using RationalFunction = std::pair<Container<Number>, Container<Number>>;
 
-		RationalFunction() = default;
-
-		RationalFunction(Container<Number> numerator, Container<Number> denominator) : num(std::move(numerator)), den(std::move(denominator)) {}
-
-		RationalFunction(const RationalFunction& other) = default;
-		RationalFunction(RationalFunction&& other) noexcept = default;
-
-		// ReSharper disable once CppNonExplicitConvertingConstructor
-		RationalFunction(std::pair<Container<Number>,Container<Number>> p) : num(std::move(p.first)), den(std::move(p.second)) {} // NOLINT(*-explicit-constructor)
-
-		RationalFunction& operator=(const RationalFunction& other) = default;
-		RationalFunction& operator=(RationalFunction&& other) noexcept = default;
-
-		RationalFunction& operator=(std::pair<Container<Number>,Container<Number>> p) {
-			num = std::move(p.first);
-			den = std::move(p.second);
-			return *this;
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<!traits::has_size<Number>::value, Number>
+	val_mul(const RationalFunction<Number, Container>& rf, Number x) {
+		Number n = 0;
+		for (const auto& c : rf.first) {
+			n = n * x + c;
 		}
+		Number d = 0;
+		for (const auto& c : rf.second) {
+			d = d * x + c;
+		}
+		return n / d;
+	}
 
-		// ReSharper disable once CppNonExplicitConversionOperator
-		operator std::pair<Container<Number>,Container<Number>>() const & { // NOLINT(*-explicit-constructor)
-			return std::make_pair(num, den);
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<traits::has_size<Container<Number>>::value, Container<Number>>
+	val_mul(const RationalFunction<Number, Container>& rf, const Container<Number>& xx) {
+		Container<Number> result(xx.size());
+#if defined(_OPENMP) && !defined(ALFI_DISABLE_OPENMP)
+#pragma omp parallel for
+#endif
+		for (SizeT i = 0; i < xx.size(); ++i) {
+			result[i] = val_mul(rf, xx[i]);
 		}
-		// ReSharper disable once CppNonExplicitConversionOperator
-		operator std::pair<Container<Number>,Container<Number>>() && noexcept { // NOLINT(*-explicit-constructor)
-			return std::make_pair(std::move(num), std::move(den));
+		return result;
+	}
+
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<!traits::has_size<Number>::value, Number>
+	val_div(const RationalFunction<Number, Container>& rf, Number x) {
+		const auto& numerator = rf.first;
+		const auto& denominator = rf.second;
+		Number n = 0;
+		for (auto i = numerator.rbegin(); i != numerator.rend(); ++i) {
+			n = n / x + *i;
 		}
-	};
+		Number d = 0;
+		for (auto i = denominator.rbegin(); i != denominator.rend(); ++i) {
+			d = d / x + *i;
+		}
+		const auto numerator_degree = numerator.empty() ? 0 : numerator.size() - 1;
+		const auto denominator_degree = denominator.empty() ? 0 : denominator.size() - 1;
+		if (numerator_degree >= denominator_degree) {
+			return n / d * util::numeric::binpow(x, numerator_degree - denominator_degree);
+		} else {
+			return n / d / util::numeric::binpow(x, denominator_degree - numerator_degree);
+		}
+	}
+
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<traits::has_size<Container<Number>>::value, Container<Number>>
+	val_div(const RationalFunction<Number, Container>& rf, const Container<Number>& xx) {
+		Container<Number> result(xx.size());
+#if defined(_OPENMP) && !defined(ALFI_DISABLE_OPENMP)
+#pragma omp parallel for
+#endif
+		for (SizeT i = 0; i < xx.size(); ++i) {
+			result[i] = val_div(rf, xx[i]);
+		}
+		return result;
+	}
+
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<!traits::has_size<Number>::value, Number>
+	val(const RationalFunction<Number, Container>& rf, Number x) {
+		if (std::abs(x) <= 1) {
+			return val_mul(rf, x);
+		} else {
+			return val_div(rf, x);
+		}
+	}
+
+	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
+	std::enable_if_t<traits::has_size<Container<Number>>::value, Container<Number>>
+	val(const RationalFunction<Number, Container>& rf, const Container<Number>& xx) {
+		Container<Number> result(xx.size());
+#if defined(_OPENMP) && !defined(ALFI_DISABLE_OPENMP)
+#pragma omp parallel for
+#endif
+		for (SizeT i = 0; i < xx.size(); ++i) {
+			result[i] = val(rf, xx[i]);
+		}
+		return result;
+	}
 }
