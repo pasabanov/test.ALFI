8869b9f1cc08328e854b78f14de81a38ca1d2982
Petr Sabanov <pasabanov@murena.io>
2025-02-16 05:03:20 +0300

    Added basic support for rational functions
    - `RationalFunction` alias for `std::pair`.
    - `val`, `val_mul`, and `val_div` functions.

--- ALFI/ALFI/util/numeric.h
+++ ALFI/ALFI/util/numeric.h
@@ -9,4 +9,22 @@ namespace alfi::util::numeric {
 	bool are_equal(Number a, Number b, Number epsilon = std::numeric_limits<Number>::epsilon()) {
 		return std::abs(a - b) <= epsilon || std::abs(a - b) <= std::max(std::abs(a), std::abs(b)) * epsilon;
 	}
+
+	template <typename Number, typename ExponentType>
+	Number binpow(Number x, ExponentType n) {
+		if constexpr (std::is_signed_v<ExponentType>) {
+			if (n < 0) {
+				return 1 / binpow(x, -n);
+			}
+		}
+		Number result = 1;
+		while (n > 0) {
+			if ((n & 1) == 1) {
+				result *= x;
+			}
+			x *= x;
+			n >>= 1;
+		}
+		return result;
+	}
 }
