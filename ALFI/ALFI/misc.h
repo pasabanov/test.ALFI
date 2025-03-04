002c88bd4da289e7dc68018557666cc5f335de25
Petr Sabanov <pasabanov@murena.io>
2025-03-04 23:58:32 +0300

    chore(barycentric): Renamed `c` (coeffs) to `W` (barycentric weights)

--- ALFI/ALFI/misc.h
+++ ALFI/ALFI/misc.h
@@ -7,7 +7,6 @@
 #include "util/numeric.h"
 
 namespace alfi::misc {
-
 	template <typename Number = DefaultNumber, template <typename, typename...> class Container = DefaultContainer>
 	Container<Number> barycentric(
 			const Container<Number>& X,
@@ -32,45 +31,46 @@ namespace alfi::misc {
 
 		const auto N = X.size();
 
-		Container<Number> c(N);
+		// barycentric weights
+		Container<Number> W(N);
 		if (dist_type == dist::Type::UNIFORM) {
-			c[0] = 1;
+			W[0] = 1;
 			for (SizeT j = 1; j <= N / 2; ++j) {
-				c[j] = -c[j-1] * (static_cast<Number>(N - j)) / static_cast<Number>(j);
+				W[j] = -W[j-1] * (static_cast<Number>(N - j)) / static_cast<Number>(j);
 			}
 			for (SizeT j = N / 2 + 1; j < N; ++j) {
-				if (c[j-1] < 0) {
-					c[j] = std::abs(c[N-1-j]);
+				if (W[j-1] < 0) {
+					W[j] = std::abs(W[N-1-j]);
 				} else {
-					c[j] = -std::abs(c[N-1-j]);
+					W[j] = -std::abs(W[N-1-j]);
 				}
 			}
 			// TODO: Investigate if normalization is needed.
-			// Normalization is needed because the middle coefficients grow as O(2^n),
+			// Normalization is needed because the middle weights grow as O(2^n),
 			// while the edges grow as O(1). Dividing by 2^(N/2) balances the exponents,
 			// reducing numerical instability.
 			// const Number norm_factor = std::pow(static_cast<Number>(2), static_cast<Number>(N / 2));
 			// for (SizeT j = 0; j < N; ++j) {
-			// 	c[j] /= norm_factor;
+			// 	W[j] /= norm_factor;
 			// }
 		} else if (dist_type == dist::Type::CHEBYSHEV) {
 			for (SizeT j = 0; j < N; ++j) {
-				c[j] = (j % 2 == 0 ? 1 : -1) * sin(((2 * static_cast<Number>(j) + 1) * M_PI) / (2 * static_cast<Number>(N)));
+				W[j] = (j % 2 == 0 ? 1 : -1) * sin(((2 * static_cast<Number>(j) + 1) * M_PI) / (2 * static_cast<Number>(N)));
 			}
 		} else if (dist_type == dist::Type::CIRCLE_PROJECTION) {
 			for (SizeT j = 0; j < N; ++j) {
-				c[j] = (j % 2 == 0 ? 1 : -1) * (j == 0 || j == N - 1 ? static_cast<Number>(1)/static_cast<Number>(2) : static_cast<Number>(1));
+				W[j] = (j % 2 == 0 ? 1 : -1) * (j == 0 || j == N - 1 ? static_cast<Number>(1)/static_cast<Number>(2) : static_cast<Number>(1));
 			}
 		} else {
-			// The factor scales the coefficients to prevent excessive growth or shrinkage, reducing numerical instability.
+			// The factor scales the weights to prevent excessive growth or shrinkage, reducing numerical instability.
 			// TODO: Investigate if normalization is needed and refine the scaling factor.
 			// const Number norm_factor = (X[N-1] - X[0]) / static_cast<Number>(2);
 			for (SizeT j = 0; j < N; ++j) {
-				c[j] = 1;
+				W[j] = 1;
 				for (SizeT k = 0; k < N; ++k) {
 					if (j != k) {
 						// c[i] *= norm_factor / (X[j] - X[k]); // with normalization
-						c[j] /= (X[j] - X[k]);
+						W[j] /= (X[j] - X[k]);
 					}
 				}
 			}
@@ -93,7 +93,7 @@ namespace alfi::misc {
 					break;
 				}
 				const auto x_diff = xx[k] - X[i];
-				const auto temp = c[i] / x_diff;
+				const auto temp = W[i] / x_diff;
 				numerator += temp * Y[i];
 				denominator += temp;
 			}
