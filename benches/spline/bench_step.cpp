c759bef813af057e9a43c5f82b519a41dfe5a987
Petr Sabanov <pasabanov@murena.io>
2025-03-24 14:35:43 +0300

    Added step spline benchmarks

---
+++ benches/spline/bench_step.cpp
@@ -0,0 +1,74 @@
+#include <benchmark/benchmark.h>
+
+#include <ALFI/spline/step.h>
+#include <ALFI/util/arrays.h>
+#include <ALFI/util/stat.h>
+
+#include "../bench_utils.h"
+#include "../bench_data.h"
+
+const std::vector<alfi::spline::StepSpline<>::Type> step_spline_types = {
+	alfi::spline::StepSpline<>::Types::Left{},
+	alfi::spline::StepSpline<>::Types::Middle{},
+	alfi::spline::StepSpline<>::Types::Right{},
+};
+
+static void BM_step_spline(benchmark::State& state) {
+	const auto& [func_name, lambda] = funcs_and_ints[state.range(0)].first;
+	const auto& interval = funcs_and_ints[state.range(0)].second;
+	const auto& dist_type = dists[state.range(1)];
+	const auto& n = state.range(2);
+	const auto& type = step_spline_types[state.range(3)];
+	const std::vector<double> X = of_type<double>(dist_type, n, interval.first, interval.second);
+	const std::vector<double> Y = apply_func(X, lambda);
+	alfi::spline::StepSpline<> spline;
+	for (auto _ : state) {
+		spline = alfi::spline::StepSpline<>(X, Y, type);
+		benchmark::DoNotOptimize(spline);
+		benchmark::ClobberMemory();
+	}
+}
+BENCHMARK(BM_step_spline)
+	->ArgsProduct({
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(funcs_and_ints.size()) - 1, 1),
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(dists.size()) - 1, 1),
+		benchmark::CreateRange(1 << 6, 1 << 21, 1 << 3),
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(step_spline_types.size()) - 1, 1),
+	})
+	->ArgNames({"func", "dist", "n", "type"});
+
+static void BM_step_spline_values(benchmark::State& state) {
+	const auto& [func_name, lambda] = funcs_and_ints[state.range(0)].first;
+	const auto& interval = funcs_and_ints[state.range(0)].second;
+	const auto& dist_type = dists[state.range(1)];
+	const auto& n = state.range(2);
+	const auto& nn = state.range(3);
+	const auto& type = step_spline_types[state.range(4)];
+	const std::vector<double> X = of_type<double>(dist_type, n, interval.first, interval.second);
+	const std::vector<double> Y = apply_func(X, lambda);
+	const std::vector<double> xx = of_type<double>(dist_type, nn, interval.first, interval.second);
+	const alfi::spline::StepSpline<> spline(X, Y, type);
+	std::vector<double> result;
+	for (auto _ : state) {
+		result = spline.eval(xx);
+		benchmark::DoNotOptimize(result);
+		benchmark::ClobberMemory();
+	}
+	const std::vector<double> yy = apply_func(xx, lambda);
+	const std::vector<double> error = alfi::util::arrays::sub(result, yy);
+	state.counters["1.ME"] = alfi::util::stat::mean(error);
+	state.counters["2.MAE"] = alfi::util::stat::mean_abs(error);
+	state.counters["3.RMSE"] = alfi::util::stat::rms(error);
+	state.counters["4.Variance"] = alfi::util::stat::var(error);
+}
+BENCHMARK(BM_step_spline_values)
+	->ArgsProduct({
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(funcs_and_ints.size()) - 1, 1),
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(dists.size()) - 1, 1),
+		concat_vectors<int64_t>(benchmark::CreateRange(8, 32, 2), benchmark::CreateRange(1 << 6, 1 << 21, 1 << 3)),
+		{nn},
+		benchmark::CreateDenseRange(0, static_cast<int64_t>(step_spline_types.size()) - 1, 1),
+	})
+	->ArgNames({"func", "dist", "n", "nn", "type"});
+
+BENCHMARK_MAIN();
