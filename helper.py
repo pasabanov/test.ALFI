17b228f22b26a38b77fff7fd6f72793d19cac5a5
Petr Sabanov <pasabanov@murena.io>
2025-11-29 20:02:30 +0300

    wip: Перешёл на CMakePresets.json

--- helper.py
+++ helper.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-import os
 import subprocess
-
-profile_directories = {
-	'Debug': 'build/debug',
-	'Release': 'build/release',
-	'RelWithDebInfo': 'build/relwithdebinfo',
-	'MinSizeRel': 'build/minsizerel',
-	'Fast': 'build/fast',
-	'FastParallel': 'build/fastparallel',
-	'Sanitize': 'build/sanitize',
-}
 
 dependencies = [
 	'build-essential',
@@ -26,24 +15,17 @@ dependencies = [
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--deps', action='store_true', help="Install dependencies")
-parser.add_argument('-b', '--build', action='store_true', help="Build project (requires profile)")
-parser.add_argument('-t', '--test', action='store_true', help='Run tests (requires profile)')
+parser.add_argument('-b', '--build', action='store_true', help="Build project (requires preset)")
+parser.add_argument('-t', '--test', action='store_true', help='Run tests (requires preset)')
 parser.add_argument('--doxygen', action='store_true', help='Generate Doxygen documentation')
-parser.add_argument('profile', nargs='?', help="Profile to build and/or test (case-insensitive)")
+parser.add_argument('preset', nargs='?', help="preset to build and/or test (case-sensitive)")
 args = parser.parse_args()
 
-if (args.build or args.test) and not args.profile:
-	parser.error("'--build' and '--test' require 'profile'")
+if (args.build or args.test) and not args.preset:
+	parser.error("'--build' and '--test' require 'preset'")
 
-if args.profile and not (args.build or args.test):
-	parser.error("'profile' requires '--build' or '--test'")
-
-profile_dir = None
-if args.profile:
-	try:
-		profile_dir = next(d for p, d in profile_directories.items() if p.lower() == args.profile.lower())
-	except StopIteration:
-		parser.error(f"Profile '{args.profile}' not found. Allowed profiles: '{"', '".join(profile_directories)}'.")
+if args.preset and not (args.build or args.test):
+	parser.error("'preset' requires '--build' or '--test'")
 
 def execute_command(command):
 	print(' '.join(command), flush=True)
@@ -54,10 +36,10 @@ if args.deps:
 	execute_command(['sudo', 'apt', 'install', '-y'] + dependencies)
 	execute_command(['git', 'submodule', 'update', '--init'])
 if args.build:
-	execute_command(['cmake', '-DCMAKE_BUILD_TYPE=' + args.profile, '-B', profile_dir])
-	execute_command(['cmake', '--build', profile_dir, '-j', str(os.cpu_count())])
+	execute_command(['cmake', '--preset', args.preset])
+	execute_command(['cmake', '--build', '--preset', args.preset, '-j'])
 if args.test:
-	execute_command(['ctest', '--test-dir', profile_dir, '--verbose'])
+	execute_command(['ctest', '--preset', args.preset])
 if args.doxygen:
 	local = 'docs/doxygen/html/mathjax/es5/'
 	remote = 'https://raw.githubusercontent.com/mathjax/MathJax/refs/tags/3.2.2/es5/'
